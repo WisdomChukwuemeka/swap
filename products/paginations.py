65f470c6e8cfb9fbb966a93f70e344ec76a8c8d2
wisdom chukwuemeka <wisdomchukwuemeka97@gmail.com>
2025-07-27 00:12:12 +0100

    auth config 1

--- products/paginations.py
+++ products/paginations.py
@@ -3,3 +3,5 @@ from rest_framework.pagination import CursorPagination
 class CustomCursorPagination(CursorPagination):
     page_size = 7  # how many items per page
     ordering = '-id'  # MUST be unique + ordered
+
+
