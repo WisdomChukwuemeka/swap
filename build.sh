34336e7e3829a9a5735801712ad16d48dc8b7b1c
wisdom chukwuemeka <wisdomchukwuemeka97@gmail.com>
2025-07-27 06:23:12 +0100

    commit

--- build.sh
+++ build.sh
@@ -6,7 +6,23 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
-if [[ "$CREATE_SUPERUSER" ]]; 
-then
-    python manage.py createsuperuser --no-input
+
+
+if [[ "$CREATE_SUPERUSER" == "true" ]]; then
+  if [[ -z "$DJANGO_SUPERUSER_USERNAME" || -z "$DJANGO_SUPERUSER_EMAIL" || -z "$DJANGO_SUPERUSER_PASSWORD" ]]; then
+    echo "Error: You must set DJANGO_SUPERUSER_USERNAME, DJANGO_SUPERUSER_EMAIL, and DJANGO_SUPERUSER_PASSWORD"
+    exit 1
+  fi
+
+  python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists():
+    User.objects.create_superuser(
+        '$DJANGO_SUPERUSER_USERNAME',
+        '$DJANGO_SUPERUSER_EMAIL',
+        '$DJANGO_SUPERUSER_PASSWORD'
+    )
+END
 fi
+
