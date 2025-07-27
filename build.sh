84a0b1cbbb93e0c535be7b792624d155601ea9cb
wisdom chukwuemeka <wisdomchukwuemeka97@gmail.com>
2025-07-27 06:44:54 +0100

    dev

--- build.sh
+++ build.sh
@@ -8,9 +8,6 @@ python manage.py migrate
 
 
 
-if [[ "$CREATE_SUPERUSER" ]]; then
-  python manage.py createsuperuser \
-    --no-input 
+if [[ $CREATE_SUPERUSER ]]; then
+    python manage.py createsuperuser --no-input
 fi
-
-
